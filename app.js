20874ebb00f9ea28fb3885bf21a8038904ac8ed9
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2020-12-09 21:39:08 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello World")
+})
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
