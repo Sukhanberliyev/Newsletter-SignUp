3ff2e318d76cadebc9c062c9b8809ec62299e697
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2020-12-10 22:05:41 +0900

    Setting up Sign Up page and posting data to Mailchimp's server via their API

--- app.js
+++ app.js
@@ -1,15 +1,68 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 
 const app = express();
 
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    res.send("Hello World")
+    res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = " https://us7.api.mailchimp.com/3.0/lists/87338b4cab";
+
+    const options = {
+        method: "POST", 
+        auth: "Aidar1:5e5bd85fed8b466d232835419c00eb11-us7"
+    }
+
+    const request = https.request(url, options, function(response) {
+        response.on("data", function() {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 })
+
+
+
+
+
+
+// API Keys
+// 5e5bd85fed8b466d232835419c00eb11-us7
+
+// List ID
+// 87338b4cab
